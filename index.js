e3835756c29cdd6ba872de0fc1cc9033acd67469
Jonathan <jonicgp97@gmail.com>
2020-08-13 19:06:51 -0500

    Separate parts of command

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const program = new Command();
 program.version('0.0.1');
 
 program
-  .command('file <file> <new-name>')
+  .command('rn')
   .description('rename a file')
+  .arguments('<file> <new_name>')
+  .version('0.0.2')
   .action((file, new_name) => {
-    console.log(`Renamed ${file} => ${new_name}`);
     fs.rename(file, new_name, function (err) {
       if (err) console.log('ERROR: ' + err);
+      else console.log(`Renamed ${file} => ${new_name}`);
     });
   })
   .parse(process.argv);
